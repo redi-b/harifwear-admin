96dfd8ebd15af3281b2afe72244ed9cf9cd32a1a
rediet <rb.rediet@gmail.com>
2025-05-06 13:44:07 +0300

    update API base URL

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_SERVER_URL,
   withCredentials: true, // Send cookies
 });
